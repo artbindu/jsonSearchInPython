380788f21ba86b89e860e103090f53f864105d31
biswasindhu mandal <biswasindhu.mandal@rebaca.com>
2019-04-30 18:04:27 +0530

    update prob02

--- config.py
+++ config.py
@@ -9,3 +9,22 @@ def prob01(searchItems) :
         return data
     except Exception:
         return ('error: ',Exception, type(Exception))
+
+
+def prob02(searchItems,searchOptions) :
+    array = []
+    try :
+        allData = json.load(open("json1.json"))
+        for data in allData :
+            # print(data)
+            for x in searchItems :
+                data = data.get(x)
+            for y in searchOptions :
+                fData = data.get(y)
+                if(fData) :
+                    array.append(fData)
+                    break
+        return array
+    except :
+        print('this is exception')
+        return('error: ',Exception, type(Exception))
--- main.py
+++ main.py
@@ -5,4 +5,13 @@ import config as config
 Search a Specific json data; like find out 'annLength from json0.json'
 '''
 data = config.prob01(['ann','annLength'])
-print('data: ', data)
+print('annLength: ', data)
+
+# ---------------problem 02-------------------------------
+'''
+find out all 'url'
+so urls lies in, mongoDbConfig --> connection
+                               --> url
+'''
+data = config.prob02(['mongoDbConfig'],['url','connection'])
+print('urls: ', data)
