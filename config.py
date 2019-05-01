2822a4b317335212f83b3d29423a7c02236897a7
biswasindhu.mandal <biswasindhu.mandal@rebaca.com>
2019-05-01 09:35:36 +0530

    prob03: mongoo new collection data insert

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import json
 
 def prob01(searchItems) :
     try :
-        data = json.load(open("json0.json"))
+        data = json.load(open("data/json0.json"))
         # print(len(data))
         for sItem in searchItems :
             data = data.get(sItem)
@@ -14,7 +14,7 @@ def prob01(searchItems) :
 def prob02(searchItems,searchOptions) :
     array = []
     try :
-        allData = json.load(open("json1.json"))
+        allData = json.load(open("data/json1.json"))
         for data in allData :
             # print(data)
             for x in searchItems :
@@ -28,3 +28,32 @@ def prob02(searchItems,searchOptions) :
     except :
         print('this is exception')
         return('error: ',Exception, type(Exception))
+
+def prob03(sourcePath, destinationPath) :
+    rfile = sourcePath
+    f = open(rfile,'r')
+    st = f.read()
+    # print(st)
+    # minLength = len(/* 2 */) = 7
+    # maxLengrh = len(/* 1100 */) = 10
+    (minLength, maxLength) = (7, 10)
+    for i in range(minLength, maxLength+1) :
+        j = 0
+        count = pow(10,(i-7))
+        while(j<len(st)) :
+            if(count == pow(10,(i-6))) :
+                print('done ',pow(10,(i-6)), ' json objects')
+                break
+            # remove '/* n */' parts for n=1,2,3,...
+            sst = '/* '+str(count)+' */'
+            if((st[j:j+i]) == sst) :
+                st = st.replace(st[j:j+i], '')
+                count += 1
+            j += 1
+    f.close()
+
+    f1 = open(destinationPath,'w')
+    f1.write(st)
+    f1.close()
+    return("paste 'json03.json' data with in 'new Mongoo Cloection'")
+    
--- main.py
+++ main.py
@@ -15,3 +15,21 @@ so urls lies in, mongoDbConfig --> connection
 '''
 data = config.prob02(['mongoDbConfig'],['url','connection'])
 print('urls: ', data)
+
+# ---------------problem 03-------------------------------
+'''
+if you have a mongoDB collection data; then how to store it again into data base.
+    Open 'Robo 3T' --> connect any machine --> create db & collection --> Insert Document (with in new Collection) --> copy a bunch of json data and paste
+    if you direct copy a mongooDB data and paste then show err: 
+                                                                Unable to parse JSON:
+                                                                Expecting '{', at (1, 1)
+                ---- because of '/* 1 */', '/* 78 */', '/* 789 */' etc
+    So, this code is to remove this portion; i.e. filtering mongoo data and store into a valid json format
+    procedure: 
+        1.  First store the mongoodata in '.txt' format
+        2.  This code read it character-by-character and remove ('/* n */'; for n=1,2,3,4...) portions
+        3.  Store output into '.json' format : copy this file and paste with in 'Insert Document' collection
+'''
+# sourcepath && destinationPath
+sms = config.prob03('data/json03.txt', 'data/json03.json')
+print(sms)
